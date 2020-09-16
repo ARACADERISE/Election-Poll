a497931a72a55988e57d47370173c6cb2cab0254
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 19:29:11 +0000

    Adding in error methods for the website

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/PollHomepage')
-def _home_():
-    return render_template('index.html', TITLE = "Election Poll App, 2020", INFORMATION = "Yes, the one poll election app!")
+@app.route('/homepage')
+def _home_(_error):
+    return render_template('index.html', TITLE = "Election Poll App, 2020", INFORMATION = "Yes, the one poll election app!", error = _error)
 
-@app.route('/set/<ideal>')
+# _figure_it_out_ will probably be for users specific requests to certain spots of the website
+@app.route('/<ideal>')
 def _figure_it_out_(ideal):
 
-    if ideal == "all_polls":
+    if ideal == "homepage":
         return redirect(url_for('_home_'))
+    else:
+        return _home_('NOO')
 
 if __name__ == '__main__':
     app.run(debug = True, port = 18080, host = '127.0.0.1')
