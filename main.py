fb8d6ebba4b5afcce2dac63cbf5c1e757d2477e0
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 18:38:09 +0000

    working on bug

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src.render_app import RenderWebpage
-
-FLASK_APP = RenderWebpage()
-FLASK_APP._setup_homepage_()
+from src.render_app import (
+    _HOMEPAGE_,
+    _RUN_
+)
 
 if __name__ == '__main__':
-    FLASK_APP._RUN_()
+    _HOMEPAGE_()
+    _RUN_()
--- src/render_app.py
+++ src/render_app.py
@@ -3,13 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 # I just got this going to get it going
-
-class RenderWebpage:
-    def __init__(self): pass # not needed yet
-
-    @app.route("/")
-    def _setup_homepage_():
-        return '</h1>Homepage</h1'
     
-    def _RUN_(self):
-        app.run(debug=True)
+
+@app.route('/')
+def _HOMEPAGE_():
+    return '<h1>Hello World</h1'
+
+def _RUN_():
+    app.run(debug=True)
