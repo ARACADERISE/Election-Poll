98c37fc7ea27b4a7627ac2727c9fea44cf8ddc9d
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 19:44:38 +0000

    /

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def _home_():
     return render_template('index.html', TITLE = "Election Poll App, 2020", INFORMATION = "Yes, the one poll election app!")
 
 # _figure_it_out_ will probably be for users specific requests to certain spots of the website
-@app.route('/<ideal>')
+@app.route('/<ideal>', METHODS = ['POST','GET'])
 def _figure_it_out_(ideal):
 
     if ideal == "homepage":
         return redirect(url_for('_home_'))
     else:
-        return render_template('error.html', ErrTitle = "URL Not Found", information = "The URL %s does not exists :(" % ideal, LINK = redirect(url_for('_home_')))
+        return render_template('error.html', ErrTitle = "URL Not Found", information = "The URL %s does not exists :(" % ideal)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 18080, host = '127.0.0.1')
