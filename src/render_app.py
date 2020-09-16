e54c47b75adf08896c3879ee947ba0b7d51b0bae
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 18:35:44 +0000

    removed quick bug

--- src/render_app.py
+++ src/render_app.py
@@ -8,7 +8,7 @@ class RenderWebpage:
     def __init__(self): pass # not needed yet
 
     @app.route("/")
-    def _setup_homepage_(self):
+    def _setup_homepage_():
         return '</h1>Homepage</h1'
     
     def _RUN_(self):
