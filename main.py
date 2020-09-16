cebe4828d6a3d8f65d909661c910a17742b0e456
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 19:22:11 +0000

    starting the app up

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from src.render_app import (
-    _HOMEPAGE_,
-    _RUN_
-)
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/PollHomepage')
+def _home_():
+    return render_template('index.html', TITLE = "Election Poll App, 2020")
+
+@app.route('/<ideal>')
+def _figure_it_out_(ideal):
+
+    if ideal == "all_polls":
+        return redirect(url_for('_home_'))
 
 if __name__ == '__main__':
-    _HOMEPAGE_()
-    _RUN_()
+    app.run(debug = True, port = 18080, host = '127.0.0.1')
+else:
+    raise Exception('\nCannot execute without __main__\n')
