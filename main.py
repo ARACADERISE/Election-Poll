96d1ba2aa166a1c82e1d258637c27d81932c8126
GetStuffDone <t.targetyt@gmail.com>
2020-09-16 19:37:20 +0000

    adding in error methods

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def _home_():
     return render_template('index.html', TITLE = "Election Poll App, 2020", INFORMATION = "Yes, the one poll election app!")
 
-@app.route('/not_found')
-def _error_(url):
-    return render_template('error.html', ErrTitle = "Error, not found :(", information = "The Url %s in was not found" % url)
-
 # _figure_it_out_ will probably be for users specific requests to certain spots of the website
 @app.route('/<ideal>')
 def _figure_it_out_(ideal):
@@ -18,7 +14,7 @@ def _figure_it_out_(ideal):
     if ideal == "homepage":
         return redirect(url_for('_home_'))
     else:
-        return redirect(url_for('_error_', url = ideal))
+        return render_template('error.html', ErrTitle = "URL Not Found", Information = "The URL %s does not exists :(" % ideal)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 18080, host = '127.0.0.1')
